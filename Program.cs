6f1bbc02cb1fae6778ace7ce9e7db253bd861b61
C.Sutter <111045656+BBBlernende@users.noreply.github.com>
2022-09-22 09:04:10 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,16 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System;
+using System.ComponentModel;
 using System.Reflection.Metadata;
 
 public class Program
 {
     public static void Main()
     {
-        Random myObject = new Random();
-        int i = myObject.Next(1, 100);
+        Random r1 = new Random();
+        int i = r1.Next(1, 100);
         int useranswer = 0;
         bool win = false;
-        bool wiederspielen = false;
+        bool wiederspielen = true;
+        int versuch = 0;
 
         try
         {
@@ -21,48 +23,93 @@ public class Program
 
                 do
                 {
+                    Console.ForegroundColor = ConsoleColor.Blue;
                     Console.Write("Erraten Sie die Zahl indem Sie hier die Zahl eingeben: ");
                     int user = Convert.ToInt32(Console.ReadLine());
+                    Console.ResetColor();
+
+                    // code falls user nummer zu gross erraten hat
 
                     if (user > i)
                     {
-                        Console.Write("Diese Zahl ist zu Hoch. (Enter drücken um fortzufahren)");
-                        Console.ReadLine();
-
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.WriteLine("Diese Zahl ist zu Hoch.");
+                        Console.ResetColor();
+                        Console.WriteLine("--------------------------------------------------");
+                        versuch++;
                     }
+
+                    // code falls user nummer zu klein erraten hat
+
                     else if (user < i)
                     {
-                        Console.Write("Diese Zahl ist zu niedrig. (Enter drücken um fortzufahren)");
-                        Console.ReadLine();
-
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.WriteLine("Diese Zahl ist zu niedrig.");
+                        Console.ResetColor();
+                        Console.WriteLine("--------------------------------------------------");
+                        versuch++;
                     }
+
+                    // code wenn user nummer erraten hat
 
                     else if (user == i)
                     {
-                        Console.Write("Super! Sie haben die Zahl erraten! ");
-                        int answer = Convert.ToInt32(Console.ReadLine());
-
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine("Super! Sie haben die Zahl erraten! ");
+                        Console.ResetColor();
                         win = true;
+                        versuch++;
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine("Sie brauchten " + versuch + " Versuche um die generierte Zahl zu erraten");
+                        Console.ResetColor();
+
+                        // frage ob user nochmal spielen möchte
+
+                        Console.Write("Möchten Sie nochmal Spielen? [y|n] ");
+                        char yorn = Convert.ToChar(Console.ReadLine());
+
+                        
+                        
+                        
+                        // loop falls ja
+
+                        if (yorn == 'y')
+                        {
+                            
+                            wiederspielen = true;
+                            i = r1.Next(1, 100);      
+                            versuch = 0;
+
+                        }
+
+                        // loop falls nein
+
+                        else
+                        {
+                            wiederspielen = false;
+                            Console.WriteLine("Vielen Danke fürs spielen. Ich wünsche Ihnen einen schönen Tag.");
+                        }
+                        
                     }
+                    
 
                 } while (win == false);
 
-                Console.WriteLine("Möchten Sie nochmal Spielen? [y|n] ");
-                char yorn = Convert.ToChar(Console.ReadLine());
-
-                if (yorn == 'y')
-                {
-                    wiederspielen = true;
-                }
-                else
-                {
-                    wiederspielen = false;
-                }
+                
 
             } while (wiederspielen);
 
         }
 
+        // code falls user buchstaben oder sonstige zeichen eingibt ausser zahlen 
+
+        catch
+        {
+            Console.ForegroundColor= ConsoleColor.Red;
+            Console.WriteLine("Eingabe Ungültig. Starten Sie das Programm bitte neu.");
+            Console.ResetColor();
+            Environment.Exit(0);
+        }
 
  
 
